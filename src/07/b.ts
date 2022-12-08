02ead8970e567b26604b905398b8e6890104bd89
Joshua Carver <mar21josh@gmail.com>
2022-12-08 22:44:04 +0000

    Completed part 2 of day 7

---
+++ src/07/b.ts
@@ -0,0 +1,202 @@
+export const x = "";
+
+const input: string = Deno.readTextFileSync("./src/07/input.txt");
+const inputArr = input.split(`\n`);
+
+interface iFSObject {
+  type: 'file' | 'directory'
+  name: string;
+}
+
+interface iDirectory extends iFSObject {
+  // children: Array<iDirectory | iFile>;
+  children: { [key: string]: iDirectory | iFile }
+}
+
+interface iFile extends iFSObject {
+  size: number;
+}
+
+const MAX_SPACE = 70000000;
+let USED_SPACE = 47870454;
+const UPDATE_SIZE = 30000000;
+let SPACE_NEEDED = 7870454;
+
+let allUniqueDirectories: { [key: string]: number } = {};
+let fileSystem: {[key: string]: iDirectory | iFile} = { '/': { type: 'directory', name: '/', children: {} }}
+let path: string[] = [];
+let currentDir = '';
+
+const navigateFileSystem = (directory: string, currentFolder: iDirectory | undefined) => {
+  if(currentFolder) {
+    return currentFolder.children[directory] as iDirectory;
+  } else {
+    let x = fileSystem['/'] as iDirectory;
+    return x.children[directory] as iDirectory;
+  }
+}
+
+const addFile = (currentDirectory: iDirectory, newFile: string) => {
+  let fileName = newFile.split(' ')[1];
+  let fileSize = newFile.split(' ')[0];
+
+  let file: iFile = {
+    type: 'file',
+    name: fileName,
+    size: parseInt(fileSize),
+  }
+
+  currentDirectory.children[fileName] = file;
+}
+
+const addDirectory = (currentDirectory: iDirectory, newDirectory: string) => {
+  let dirName = newDirectory.split(' ')[1];
+  
+  let directory: iDirectory = {
+    type: 'directory',
+    name: dirName,
+    children: {}
+  }
+  
+  currentDirectory.children[dirName] = directory;
+  let key = path.join('|') + `|${dirName}`;
+  allUniqueDirectories[key] = 0;
+}
+
+const sumDirChildrenSizeRecursively = (currentDir: iDirectory) => {
+  let totalSize = 0;
+  
+  let keys = Object.keys(currentDir.children);
+  keys.forEach((key) => {
+    let dirChild = currentDir.children[key];
+    if(dirChild.type === 'directory') {
+      let size = sumDirChildrenSizeRecursively(dirChild as iDirectory);
+      totalSize = totalSize + size;
+    }
+
+    if(dirChild.type === 'file') {
+      totalSize = totalSize + (dirChild as iFile).size;
+    }
+  })
+  return totalSize;
+}
+
+const interpret = (input: string) => {
+  if(input.startsWith('$')) {
+    // User Input
+    if(input.startsWith('$ cd')) {
+      if(input.split(' ')[2] === '..') {
+        // GO UP a dir
+        currentDir = path.pop() as string;
+      } else {
+        // GO INTO a dir
+        currentDir = input.split(' ')[2]
+        path.push(currentDir);
+      }
+    }
+    
+  } else {
+    // Machine Output
+    if(input.startsWith('$ ls')) {
+      // LISTING Contents
+      // We don't need to do anything here
+      // our other Machine outputs will ingest the files and dirs
+    }
+    if(input.startsWith('dir ')) {
+      // NESTED DIR
+      let tempPath = [...path];
+      tempPath.shift();
+      let fileExplorer: iDirectory = fileSystem['/'] as iDirectory;
+
+      while(tempPath.length > 0) {
+        let next = tempPath.shift() as string;
+        fileExplorer = navigateFileSystem(next, fileExplorer);
+      }
+
+      addDirectory(fileExplorer, input);
+    } else {
+      // NESTED FILE
+      let tempPath = [...path];
+      tempPath.shift();
+      let fileExplorer: iDirectory = fileSystem['/'] as iDirectory;
+
+      while(tempPath.length > 0) {
+        let next = tempPath.shift() as string;
+        fileExplorer = navigateFileSystem(next, fileExplorer);
+      };
+
+      addFile(fileExplorer, input);
+    }
+  }
+}
+
+// Iterate through input commands, convert to tree structure
+inputArr.forEach(i => interpret(i));
+
+let underTenK: { [key: string]: string } = {};
+
+// Gather All Unique directories sizes and associate the sizes with the existing object
+Object.keys(allUniqueDirectories).forEach(uniqueDirKey => {
+  let tempDirPath = uniqueDirKey.split('|');
+  tempDirPath.shift();
+  let fileExplorer = fileSystem['/'] as iDirectory;
+
+  while(tempDirPath.length > 0) {
+    let next = tempDirPath.shift() as string;
+    fileExplorer = navigateFileSystem(next, fileExplorer);
+  }
+
+  let sum = sumDirChildrenSizeRecursively(fileExplorer);
+  allUniqueDirectories[uniqueDirKey] = sum;
+  if(sum < 100000) {
+    underTenK[uniqueDirKey] = sum;
+  }
+});
+
+console.log(JSON.stringify(fileSystem));
+console.log();
+
+console.log('All Unique Directories');
+console.log(allUniqueDirectories);
+console.log();
+
+console.log('All Unique Directories Under 100,000 bytes in size');
+console.log(underTenK)
+console.log();
+
+console.log('Sum of All Unique Directories Under 100,000 bytes in size');
+let sum = 0;
+Object.keys(underTenK).forEach(k => {sum = sum + underTenK[k]})
+console.log(sum)
+console.log();
+
+console.log('Sum of all Directories contents');
+USED_SPACE = sumDirChildrenSizeRecursively(fileSystem['/'] as iDirectory);
+console.log(USED_SPACE);
+console.log();
+
+console.log('Free space on Device');
+console.log(MAX_SPACE - USED_SPACE); // 22,129,546
+console.log();
+
+console.log('Need to Free this amount of space for the update');
+SPACE_NEEDED = UPDATE_SIZE - (MAX_SPACE - USED_SPACE)
+console.log(SPACE_NEEDED); // 7,870,454
+console.log(); 
+
+let candidates: { [key: string]: number } = {};
+Object.keys(allUniqueDirectories).forEach(key => {
+  if(allUniqueDirectories[key] > SPACE_NEEDED) candidates[key] = allUniqueDirectories[key];
+});
+
+let lowest = MAX_SPACE;
+Object.keys(candidates).forEach(i => {
+  if(candidates[i] < lowest) lowest = candidates[i];
+});
+
+console.log('The Smallest Dir we can delete and have enough space for the update');
+console.log(lowest);
+console.log(); 
+
+Deno.writeTextFile("./src/07/output.json", JSON.stringify(fileSystem, undefined, 2));
+
